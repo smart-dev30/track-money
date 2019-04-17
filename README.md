7d0f4ccd828937d71b4633b342184fc212815e7c
Markus Bjerke <markbjerke.sln@gmail.com>
2019-04-17 11:34:33 +0300

    money tracker source code

---
+++ README.md
@@ -0,0 +1,13 @@
+# Money Tracker
+
+[![Money Tracker](https://moneytracker.cc/assets/screenshot.png)](https://app.moneytracker.cc)
+
+**WORK IN PROGRESS**
+
+Money Tracker is a progressive web app that allows you to track your income and expenses.
+
+This app can work offline on desktop, tablet and mobile.
+
+Data is stored locally on device in [PouchDB](https://pouchdb.com/) database and can be synced to the cloud.
+
+Implemented with [React](https://github.com/facebook/react) and [Semantic UI](https://github.com/Semantic-Org/Semantic-UI-React)
---
+++ src/containers/Settings/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import CurrencyInput from './Currency/Input';
+import CurrenchExchangeRate from './Currency/ExchangeRate';
+import DataImport from './DataImport';
+import User from './User';
+import CollapsibleSection from '../../components/CollapsibleSection';
+
+const Settings = () => (
+  <div className="container-full-page mt-settings">
+    <CollapsibleSection name="settings_currency" label="Currency">
+      <CurrencyInput />
+      <CurrenchExchangeRate />
+    </CollapsibleSection>
+    <CollapsibleSection name="settings_import" label="Data Import">
+      <DataImport />
+    </CollapsibleSection>
+    <CollapsibleSection name="settings_user" label="User">
+      <User />
+    </CollapsibleSection>
+  </div>
+);
+
+export default Settings;
